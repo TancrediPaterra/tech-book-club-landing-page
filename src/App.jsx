d555bcddc8374fec632cab946e02332437b9503f
Tancredi.Paterra <tancredi.paterra@gmail.com>
2025-02-22 17:49:30 +0100

    aggiunti styled component e global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
+import styled from 'styled-components';
+import GlobalStyles from './components/GlobalStyles';
 
+function App() {
+  return (
+    <Wrapper>
+
+      <GlobalStyles />
+    </Wrapper>
+  );
+}
+
+export default App;
+
+const Wrapper = styled.div`
+`;
